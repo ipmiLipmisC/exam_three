1f826dae4dc8b605b6b8306a3540c4463a28228c
ipmiLipmisC <doracserenyec@gmail.com>
2022-11-09 18:32:47 +0100

    sdjbfs,jfbjm

--- script.js
+++ script.js
@@ -1,55 +1,80 @@
+const search = document.createElement('input')
+search.id = "search"
+search.setAttribute('placeholder', 'search')
+
+document.getElementById('root').append(search)
+
+const table = document.createElement('div')
+table.id = "cards"
+document.getElementById('root').append(table)
+
+const whileLoad = document.createElement('div')
+whileLoad.innerText = "Loading..."
+document.getElementById('root').append(whileLoad)
+
+
+
 const getUsers = async () => {
-    const request = await fetch('https://api.github.com/users');
-    const requestInfos = await request.json();
-    console.log(requestInfos)
-
-    let userImg = document.querySelector('img')
-    let name = document.getElementById('name')
-    let btn = document.querySelector('button')
-    let span1 = document.getElementById('1')
-    let span2 = document.getElementById('2')
-
-
-// requestInfos.map(user => {
-//     `<img src=${user.avatar_url}/>
-//     <span id="name">${user.login}</span>
-//     <button>Show more</button>
-//     <span id="plus" class="plusInfo">
-//                 <span id ="1"></span>
-//                 <span id ="2"></span></span>
-// })
-
-
-
-    for (let user of requestInfos) {
-        userImg.setAttribute('src', user.avatar_url)
-        name.innerText = user.login
-        span1.innerText = "Rank:" + user.type
-        span2.innerText = "Admin:" + user.site_admin
-
-        //     container.insertAdjacentHTML = ("afterbegin", `
-        //     <input type="text" placeholder="Search">
-        //     <div id="table">
-        //         <div id="user">
-        //             <img src="${user.avatar_url}" />
-        //             <span id="name">${user.login}</span>
-        //             <button>Show more</button>
-        //             <span class="plusInfo">Rank:${user.type}</span>
-        //             <span class="plusInfo">Admin:${user.site_admin}</span>
-        //         </div>
-        //     </div>
-        //     `
-        //     )
-        // }
-
-        btn.addEventListener('click', showInfo)
-
-        function showInfo() {
-            let showBtn = document.getElementById('plus').style.display = "block";
-
-
-        }
-    }
+    let response = await fetch('https://api.github.com/users');
+    let data = await response.json();
+    // console.log(data)
+    return data
 }
-getUsers()
+
+const createCardForUserInfo = (user) => {
+    const card = document.createElement("div")
+    card.classList.add("card")
+
+    const image = document.createElement("img")
+    image.src = user.avatar_url
+    card.append(image)
+
+    const name = document.createElement("p")
+    name.innerText = user.login
+    card.append(name)
+
+    const button = document.createElement("button")
+    button.innerText = "Show more"
+    card.append(button)
+
+    const plusData = document.createElement("div")
+    const rank = document.createElement("p")
+    rank.innerText = `Rank: ${user.type}`
+    plusData.append(rank)
+    const admin = document.createElement("p")
+    admin.innerText = `Admin: ${user.site_admin}`
+    plusData.append(admin)
+
+    plusData.style.display = "none"
+    card.append(plusData)
+    table.append(card)
+}
+
+const renderUsers = async () => {
+    let users = await getUsers()
+    console.log(users)
+    users.forEach((user) => createCardForUserInfo(user))
+    whileLoad.style.display = "none"
+    buttonHandler()
+  }
+  
+  
+  const showPlusData = (e) => {
+    const button = e.target
+    if (button.innerText === "Show more") {
+      button.innerText = "Show less"
+      button.nextSibling.style.display = "block"
+    }
+    else {
+      button.innerText = "Show more"
+      button.nextSibling.style.display = "none"
+    }
+  } 
+const buttonHandler = () => {
+    const buttons = document.querySelectorAll("button")
+    buttons.forEach((button) => {
+      button.addEventListener("click", showPlusData)
+    })
+  }
+  window.addEventListener("load", renderUsers)
 
